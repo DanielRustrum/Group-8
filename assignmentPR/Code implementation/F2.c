8a0f92c784606efdfcc701c48987e3c66335fc3a
ian ambos <ian.ambos@yahoo.com>
2020-09-07 16:15:40 -0700

    patch 01: Filled F2.h (doesn't seem like it needs much) and created characteristic and mantissa add function (needs testing)

--- assignmentPR/Code implementation/F2.c
+++ assignmentPR/Code implementation/F2.c
@@ -1,1 +1,113 @@
-//temp 
+#include "F2.h"
+
+
+
+//--------------Main program---------------\\
+
+
+
+
+
+
+//-------------Functions---------------\\
+
+//takes in two sets of mantissa and characteristic, a char array to store
+//result in and the length of the array.
+
+//c = characteristic
+//n & d = numerator and denominator of characteristic
+//Ex: 0.352 -> c = 0, n = 351 & d = 1000
+
+//Note: result data will be lost if array is passed in anonymously
+bool add(int c1, int n1, int d1,
+         int c2, int n2, int d2,
+         char result[], int len)
+{
+  //base case, not enough to store characteristic
+  if(len <= 1)
+  {
+    return false;
+  }
+
+  int sizeNeeded = ((n1 + n2) / 10) + 1;
+  int resultArrindex = 0;
+  int tempArrIndex = 0;
+  int numLSD1;
+  int numLSD2;
+  int numResult;
+  int tempResult[sizeNeeded];
+  int charResult = c1 + c2;
+  bool carry = false;
+
+
+
+  result[resultArrindex] = (char) charResult;
+  resultArrindex++;
+  result[resultArrindex] = '.';
+
+  //loop while denominators are not zero, takes off the end of each
+  //numerator, then adds them
+  //update: divide denominators by 10
+  while(d1 > 0 || d2 > 0)
+  {
+
+    numLSD1 = n1 % 10;
+    numLSD2 = n2 % 10;
+
+    numResult = numLSD1 + numLSD2;
+
+    if(carry)
+    {
+      numResult++;
+      carry = false;
+    }
+
+    if(numResult > 9)
+    {
+      numResult %= 10;
+      carry = true;
+    }
+
+    tempResult[tempArrIndex] = numResult;
+
+    tempArrIndex++;
+    d1 /= 10;
+    d2 /= 10;
+
+    n1 /= 10;
+    n2 /= 10;
+
+  }
+
+  while(resultArrIndex < len - 1)
+  {
+
+    result[resultArrIndex] = (char) tempResult[tempArrIndex];
+
+    resultArrIndex++;
+    tempArrIndex--;
+
+  }
+
+  result[resultArrIndex] = '\0';
+
+  return true;
+
+}
+
+//takes in two sets of mantissa and characteristic, a char array to store
+//result in and the length of the array.
+
+//c = characteristic
+//n & d = numerator and denominator of characteristic
+//Ex: 0.352 -> c = 0, n = 351 & d = 1000
+
+//Note: result will be lost if array is passed in anonymously
+bool subtract(int c1, int n1, int d1,
+              int c2, int n2, int d2,
+              char result[], int len)
+{
+
+
+
+}
---
+++ assignmentPR/Code implementation/F2.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+//--------------Function prototypes-----------------\\
+bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);  
